3b602ba340d980cddfdfe9fea767445f4a4d4083
JButter <doubenitamara@gmail.com>
2018-04-13 19:53:00 +0100

    restructure the game view made change to class PlayGame

--- app/src/main/java/botamtech/com/tictactoe/PlayGame.java
+++ app/src/main/java/botamtech/com/tictactoe/PlayGame.java
@@ -17,27 +17,37 @@ public class PlayGame extends AppCompatActivity implements View.OnClickListener 
     private TextView player1_text_view;
     private TextView player2_text_view;
     private TextView turn_text_view;
-    private int row, col;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-
+        /**
+         * retrieve the data sent via intent
+         */
         Intent intent = getIntent();
-
         boardSize = Integer.parseInt(intent.getExtras().getString("board size"));
         singlePlayer = intent.getExtras().getBoolean("single player");
         player1_name = intent.getExtras().getString("player 1 name");
 
+        /**
+         * check if the parameter passed via intent is
+         * single user with computer or two users
+         */
         if (singlePlayer == false)
             player2_name = intent.getExtras().getString("player 2 name");
         else {
-            player2_name = intent.getExtras().getString("player 1 name");
-            player1_name = "Computer";
+            player1_name = intent.getExtras().getString("player 1 name");
+            player2_name = "Computer";
         }
 
 
+        /**
+         * check for board size
+         * switch via to the proper value pass
+         * the content view is set to the number of
+         * board size retrieved via intent
+         */
         if (boardSize == 3)
             setContentView(R.layout.activity_play_game_3);
 
@@ -47,7 +57,9 @@ public class PlayGame extends AppCompatActivity implements View.OnClickListener 
         if (boardSize == 5)
             setContentView(R.layout.activity_play_game_5);
 
-
+        /**
+         * initializationof textview
+         */
         player1_text_view = (TextView) findViewById(R.id.player1_text_view);
         player2_text_view = (TextView) findViewById(R.id.player2_text_view);
         turn_text_view = (TextView) findViewById(R.id.turn_text_view);
@@ -56,12 +68,22 @@ public class PlayGame extends AppCompatActivity implements View.OnClickListener 
         player2_text_view.setText(player2_name + ":0");
         turn_text_view.setText(player1_name + "'s turn(X)");
 
+        /**
+         * creating and initilizing two dimensional array
+         * corresponding to the board size received
+         */
         Button[][] playBoard = new Button[boardSize][boardSize];
 
         for (int row = 0; row < boardSize; row++) {
 
             for (int col = 0; col < boardSize; col++) {
 
+                /**
+                 * geting each unique button id from the
+                 * content view and initizing each button of
+                 * the two dimensional arrays of
+                 * the button with the id
+                 */
                 String btn_Id = "btn_" + row + col;
                 int resource_Id = getResources().getIdentifier(btn_Id, "id", getPackageName());
 
@@ -78,20 +100,11 @@ public class PlayGame extends AppCompatActivity implements View.OnClickListener 
                 resetGameBoard();
             }
         });
-       /* do{*/
-      /*  if (singlePlayer == true) {
-            player2_text_view.setText(player1_name + ": 0");
-            computerPlay(playBoard);
-            turn_text_view.setText(player2_name + "'s turn(O)");
-        }
-*/
-
-        if (singlePlayer == true) {
-            computerPlay();
-            turn_text_view.setText(player2_name + "'s turn(O)");
-        }
     }
 
+    /**
+     * reset playBoard of the game
+     */
     private void resetGameBoard() {
 
         Button[][] playBoard = new Button[boardSize][boardSize];
@@ -130,6 +143,10 @@ public class PlayGame extends AppCompatActivity implements View.OnClickListener 
     }
 
 
+    /**
+     * this method is called when
+     * player 1 wins the game
+     */
     private void player1Wins() {
 
         player1Points++;
@@ -138,6 +155,10 @@ public class PlayGame extends AppCompatActivity implements View.OnClickListener 
         // resetGameBoard();
     }
 
+    /**
+     * this method is called when
+     * player 2 wins the game
+     */
     private void player2Wins() {
         player2Points++;
         turn_text_view.setText(player2_name + " wins!");
@@ -145,17 +166,22 @@ public class PlayGame extends AppCompatActivity implements View.OnClickListener 
         //resetGameBoard();
     }
 
+    /**
+     * this method updates the scores of each
+     * player in the playBoard of the game
+     */
     private void updatePointsTextView() {
         player1_text_view.setText(player1_name + ": " + player1Points);
         player2_text_view.setText(player2_name + ": " + player2Points);
         //  turn_text_view.setText(player1_name + "'s turn(X)");
     }
 
+    //this method is called when there is a draw
     private void draw() {
         turn_text_view.setText("Game Draw!");
     }
 
-
+    //this method is called to check for winner
     private boolean checkForWin() {
 
         Button[][] playBoard = new Button[boardSize][boardSize];
@@ -184,6 +210,11 @@ public class PlayGame extends AppCompatActivity implements View.OnClickListener 
         }
         if (boardSize == 3) {
 
+            /**
+             * checking rows and col to see if there
+             * is a match for the current player
+             * for X or O
+             */
             for (int i = 0; i < boardSize; i++) {
                 if (field[i][0].equals(field[i][1]) && field[i][0].equals(field[i][2])
                         && !field[i][0].equals("")) {
@@ -198,6 +229,11 @@ public class PlayGame extends AppCompatActivity implements View.OnClickListener 
                 }
             }
 
+            /**
+             * checking diagonals to see if there
+             * is a match for the current player
+             * for X or O
+             */
             if (field[0][0].equals(field[1][1]) && field[0][0].equals(field[2][2])
                     && !field[0][0].equals("")) {
                 return true;
@@ -269,10 +305,15 @@ public class PlayGame extends AppCompatActivity implements View.OnClickListener 
         return false;
     }
 
+
     @Override
     protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
-
+        /**
+         * saving roundCount, player one point and player two points
+         * the boolean indicating if is player time to play
+         * using the onSaveInstanceState
+         */
         outState.putInt("roundCount", roundCount);
         outState.putInt("player1Points", player1Points);
         outState.putInt("player2Points", player2Points);
@@ -284,6 +325,11 @@ public class PlayGame extends AppCompatActivity implements View.OnClickListener 
     protected void onRestoreInstanceState(Bundle savedInstanceState) {
         super.onRestoreInstanceState(savedInstanceState);
 
+        /**
+         * retrieving roundCount, player one point and player two points
+         * the boolean indicating if is player time to play
+         * using the onSaveInstanceState
+         */
         roundCount = savedInstanceState.getInt("roundCount");
         player1Points = savedInstanceState.getInt("player1Points");
         player2Points = savedInstanceState.getInt("player2Points");
@@ -312,28 +358,37 @@ public class PlayGame extends AppCompatActivity implements View.OnClickListener 
             updateAfterCheckWin();
         } else {
             // if (!player1Turn) {
+            roundCount++;
+            boolean first_play = false;
+
             ((Button) v).setText("O");
             turn_text_view.setText(player1_name + "'s turn(X)");
+            if (checkForWin()) {
+                player1Wins();
+                first_play = true;
+            }
 
-            new Handler().postDelayed(new Runnable() {
-                @Override
-                public void run() {
-                    computerPlay();
-                    turn_text_view.setText(player2_name + "'s turn(O)");
+            if (first_play == false) {
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        computerPlay();
+                        turn_text_view.setText(player2_name + "'s turn(O)");
+                    }
+                }, 1500);
+                roundCount++;
+                if (checkForWin()) {
+                    player2Wins();
+                    first_play = true;
                 }
-            }, 1000);
+            }
 
-            updateAfterCheckWin();
-        }/*else {
-                computerPlay();
-                turn_text_view.setText(player1_name + "'s turn(X)");
-
-            }*/
-           /* computerPlay();
-            turn_text_view.setText(player1_name + "'s turn(X)");
-            updateAfterCheckWin();*/
-        // }
-
+            if (roundCount == (boardSize * boardSize)) {
+                first_play = false;
+                roundCount = 0;
+                draw();
+            }
+        }
     }
 
     /**
@@ -357,38 +412,39 @@ public class PlayGame extends AppCompatActivity implements View.OnClickListener 
     }
 
     private void computerPlay() {
-        new Handler().postDelayed(new Runnable() {
+
+        /**
+         * get all the data from the views
+         * for use
+         */
+        final Button[][] playBoard = new Button[boardSize][boardSize];
+
+        for (int row = 0; row < boardSize; row++) {
+
+            for (int col = 0; col < boardSize; col++) {
+
+                String btn_Id = "btn_" + row + col;
+                int resource_Id = getResources().getIdentifier(btn_Id, "id", getPackageName());
+
+                playBoard[row][col] = (Button) findViewById(resource_Id);
+
+            }
+        }
+
+        int randCount = (boardSize * boardSize);
+        int random = (int) (Math.random() * randCount);
+        /**
+         * generate random numbers for the row and col
+         */
+        final int row = random / boardSize;
+        final int col = random % boardSize;
+
+        new Handler().post(new Runnable() {
 
             @Override
             public void run() {
 
-                /**
-                 * get all the data from the views
-                 * for use
-                 */
-                final Button[][] playBoard = new Button[boardSize][boardSize];
-
-                for (int row = 0; row < boardSize; row++) {
-
-                    for (int col = 0; col < boardSize; col++) {
-
-                        String btn_Id = "btn_" + row + col;
-                        int resource_Id = getResources().getIdentifier(btn_Id, "id", getPackageName());
-
-                        playBoard[row][col] = (Button) findViewById(resource_Id);
-
-                    }
-                }
-
-                int randCount = (boardSize * boardSize);
-                int random = (int) (Math.random() * randCount);
-                /**
-                 * generate random numbers for the row and col
-                 */
-                row = random / boardSize;
-                col = random % boardSize;
-
-                if (player1Turn && !(playBoard[row][col].getText().equals("X")) && !(playBoard[row][col].getText().equals("O"))) {
+                if (!(playBoard[row][col].getText().equals("X")) && !(playBoard[row][col].getText().equals("O"))) {
 
                     playBoard[row][col].performClick();
                     playBoard[row][col].setText("X");
@@ -398,6 +454,6 @@ public class PlayGame extends AppCompatActivity implements View.OnClickListener 
                     computerPlay();
                 }
             }
-        }, 200);
+        });
     }
 }
